70c13e1ad7f0862169d929ea07c54fb53d8136f1
onyxvillamor <awsgegud420@gmail.com>
2024-08-04 00:56:35 +0800

    added new migrations and models

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,26 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
+use App\Models\Twutter;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
+        // $twutter = new Twutter([
+        //     'content' => 'sheeshableshaa'
+        // ]);
 
-        $users = [
-            [
-                'name' => 'Alex',
-                'age' => '21'
-            ],
-            [
-                'name' => 'Charlie',
-                'age' => '29'
-            ],
-            [
-                'name' => 'Kendra',
-                'age' => '17'
-            ]
-        ];
-        return view('dashboard',['users' => $users]);
+        // $twutter->save();
+
+        return view('dashboard', [
+            'twutters' => Twutter::orderBy('created_at', 'DESC')->get()
+        ]);
     }
 }
