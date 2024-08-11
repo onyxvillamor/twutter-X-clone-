318e4577f75a6471879f2ade82a6ea90efa3feda
onyxvillamor <awsgegud420@gmail.com>
2024-08-12 01:13:55 +0800

    minor changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,34 +1,32 @@
-
 @extends('layout.layout')
 
 @section('content')
-<div class="row">
-    <div class="col-3">
-        @include('shared.sidebar')
-    </div>
-    <div class="col-6">
-        @include('shared.success-message')
-        @include('shared.submit-content')
-
-        <hr>
-        @foreach ($twutters as $content)
-        <div class="mt-3">
-            @include('shared.twutter-card')
+    <div class="row">
+        <div class="col-3">
+            @include('shared.sidebar')
         </div>
-        @endforeach
-        <div class="mt-2">
-            {{ $twutters->links() }}
+        <div class="col-6">
+            @include('shared.success-message')
+            @include('shared.submit-content')
+
+            <hr>
+
+            @forelse ($twutters as $content)
+                <div class="mt-3">
+                    @include('shared.twutter-card')
+                </div>
+            @empty
+                <h3 class="text-center">No Results Found.</h3>
+            @endforelse
+
+            <div class="mt-2">
+                {{ $twutters->withQueryString()->links() }}
+            </div>
         </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
 
-
+        </div>
     </div>
-    <div class="col-3">
-        @include('shared.search-bar')
-        @include('shared.follow-box')
-
-    </div>
-</div>
-
-
 @endsection
-
--- resources/views/shared/search-bar.blade.php
+++ resources/views/shared/search-bar.blade.php
@@ -4,7 +4,7 @@
     </div>
     <div class="card-body">
         <form action="{{ route('dashboard') }}" method="GET">
-            <input placeholder="..." class="form-control w-100" type="text" name="search">
+            <input placeholder="..." value="{{ request('search', '') }}" class="form-control w-100" type="text" name="search">
             <button class="btn btn-dark mt-2"> Search</button>
         </form>
     </div>
